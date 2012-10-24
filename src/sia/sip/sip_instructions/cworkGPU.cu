4006a2309d093f592972430282fec84a3d3782b3
Beverly Sanders <sanders@cise.ufl.edu>
2012-10-24 11:28:05 -0400

    Saving shauns uncomitted changes  On branch smcdowelGPU  Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	modified:   GNUmakefile.chssi
    	modified:   INSTALL
    	modified:   configure
    	modified:   configure.ac
    	modified:   src/main/Makefile.in
    	modified:   src/sia/sip/sip_instructions/Makefile.in
    	modified:   src/sia/sip/sip_instructions/contract_blocks.F
    	modified:   src/sia/sip/sip_instructions/cworkGPU.cu
    	modified:   src/sia/sip_shared/init_gpu.c
    	modified:   src/sial_compiler/compiler/Makefile.in
    	modified:   test_compare/Makefile.in
     Untracked files: (not included in commit, but changed. stored in tgz files
    	Makefile
    	bin/test_results
    	bin/xaces3
    	bin/xtest_compare
    	config.h
    	config.log
    	config.status
    	confscript.sh
    	lib/libaces2.a
    	lib/libdup.a
    	lib/liberd.a
    	lib/libframelib.a

--- src/sia/sip/sip_instructions/cworkGPU.cu
+++ src/sia/sip/sip_instructions/cworkGPU.cu
@@ -9,299 +9,252 @@
 // Configuration
 //**************************************************
 #define MAX_DIMS 20
-#define SCRATCH_BUFFER_SIZE_MB 256
-#define REORDER_TRANSFER_SIZE_MB 40
-#define REORDER_STREAMS 32
+#define SCRATCH_BUFFER_SIZE_MB 100
 #define REORDER_BLOCKS 48
 #define REORDER_THREADS 512
 
-
+void _alloc();
+void _free();
+void _cwork(f_double*  y, f_int ny, f_int*  yDims, f_int*  yInds,
+	    f_double* x1, f_int n1, f_int* x1Dims, f_int* x1Inds,
+	    f_double* x2, f_int n2, f_int* x2Dims, f_int* x2Inds); 
 //**************************************************
 // Global Pointers
 //**************************************************
-double* scratch1;
-double* scratch2;
-double* scratch3;
+f_double* scratch1;
+f_double* scratch2;
+f_double* scratch3;
 
-__constant__ int ldDev[MAX_DIMS];
-__constant__ int stepDev[MAX_DIMS];
+__constant__ f_int dimsDev[MAX_DIMS];
+__constant__ f_int stepsDev[MAX_DIMS];
 
 cublasHandle_t cublasHandle;
-cudaStream_t reorderStreams[REORDER_STREAMS];
 
-//**************************************************
-// Procedures
-//**************************************************
-
-#define imin(a, b) ((a < b)? a : b)
-
-#ifdef __cplusplus
 extern "C" {
-#endif
-void F77_NAME(cwork_gpu_alloc, CWORK_GPU_ALLOC)() {
-  cudaMalloc(&scratch1, SCRATCH_BUFFER_SIZE_MB * 1024 * 1024);
-  cudaMalloc(&scratch2, SCRATCH_BUFFER_SIZE_MB * 1024 * 1024);
-  cudaMalloc(&scratch3, SCRATCH_BUFFER_SIZE_MB * 1024 * 1024);
+  void F77_NAME(cwork_gpu_alloc, CWORK_GPU_ALLOC)() {
+    _alloc();
+  }
+}
+
+extern "C" {
+  void F77_NAME(cwork_gpu_free, CWORK_GPU_FREE)() {
+    _free();
+  }
+}
+
+extern "C" {
+  void F77_NAME(cwork_gpu, CWORK_GPU)
+       (f_double*  y, f_int* ny, f_int*  nya, f_int* nyb, f_int*  yInds,
+	f_double* x1, f_int* n1, f_int* nx1a, f_int* nx1b, f_int* x1Inds,
+	f_double* x2, f_int* n2, f_int* nx2a, f_int* nx2b, f_int *x2Inds) {
+    f_int yDims[MAX_DIMS], x1Dims[MAX_DIMS], x2Dims[MAX_DIMS];
+    int i;
+    for(i = 0; i < MAX_DIMS; i++) {
+      yDims[i] = nyb[i] - nya[i] + 1;
+      x1Dims[i] = nx1b[i] - nx1a[i] + 1;
+      x2Dims[i] = nx2b[i] - nx2a[i] + 1;
+    }
+    _cwork( y, *ny,  yDims,  yInds,
+	   x1, *n1, x1Dims, x1Inds,
+	   x2, *n2, x2Dims, x2Inds);
+  }
+}
+
+void _alloc() {
+  int dev;
+  if(cudaGetDevice(&dev))
+    printf("GPU ERROR: cworkGPU: alloc: getDevice\n");
+  printf("Allocating GPU memory on device %d\n", dev);
+  
+  if(cudaMalloc(&scratch1, SCRATCH_BUFFER_SIZE_MB * 1024 * 1024) ||
+     cudaMalloc(&scratch2, SCRATCH_BUFFER_SIZE_MB * 1024 * 1024) ||
+     cudaMalloc(&scratch3, SCRATCH_BUFFER_SIZE_MB * 1024 * 1024))
+    printf("GPU ERROR: cworkGPU: alloc: cudaMalloc\n");
+  
   cublasCreate(&cublasHandle);
+}
 
-  for(int i = 0; i < REORDER_STREAMS; i++)
-    cudaStreamCreate(&reorderStreams[i]);
-}
-}
-#ifdef __cplusplus
-extern "C" {
-#endif
-void F77_NAME(cwork_gpu_free, CWORK_GPU_FREE)() {
+void _free() {
   cudaFree(scratch1);
   cudaFree(scratch2);
   cudaFree(scratch3);
   cublasDestroy(cublasHandle);
+}
 
-  for(int i = 0; i < REORDER_STREAMS; i++)
-    cudaStreamDestroy(reorderStreams[i]);
-}
-}
-__global__ void reorderScatter(double* newX, double* oldX, int ndims, int size, int offset = 0) {  
-  int step = gridDim.x * blockDim.x;
-  int oldIndex = blockIdx.x * blockDim.x + threadIdx.x + offset;
+__global__ void reorderScatter(double* newX, double* oldX, int ndims, int size) {  
+  int blockstep = gridDim.x * blockDim.x;
+  int oldIndex = blockIdx.x * blockDim.x + threadIdx.x;
   int newIndex;
+  int t;
   int i;
 
   while(oldIndex < size) {
-    newIndex = oldIndex / ldDev[0] * stepDev[0];
-    for(i = 1; i < ndims; i++)
-      newIndex += (oldIndex % ldDev[i-1]) / ldDev[i] * stepDev[i];    
+    t = oldIndex;
+
+    newIndex = t % dimsDev[0] * stepsDev[0];
+    t /= dimsDev[0];
+
+    for(i = 1; i < ndims; i++) {
+      newIndex += t % dimsDev[i] * stepsDev[i];
+      t /= dimsDev[i];
+    }
 
     newX[newIndex] = oldX[oldIndex];
-    oldIndex += step;
+    oldIndex += blockstep;
   }
 }
 
-__global__ void reorderGather(double* newX, double* oldX, int ndims, int size, int offset = 0) {
-  int step = gridDim.x * blockDim.x;
-  int newIndex = blockIdx.x * blockDim.x + threadIdx.x + offset;
+__global__ void reorderGather(double* newX, double* oldX, int ndims, int size) {
+  int blockstep = gridDim.x * blockDim.x;
+  int newIndex = blockIdx.x * blockDim.x + threadIdx.x;
   int oldIndex;
+  int t;
   int i;
 
   while(newIndex < size) {
-    oldIndex = newIndex / ldDev[0] * stepDev[0];
-    for(i = 1; i < ndims; i++)
-      oldIndex += (newIndex % ldDev[i-1]) / ldDev[i] * stepDev[i];
+    t = newIndex;
+    oldIndex = t % dimsDev[0] * stepsDev[0];
+    t /= dimsDev[0];
+
+    for(i = 1; i < ndims; i++) {
+      oldIndex += t % dimsDev[i] * stepsDev[i];
+      t /= dimsDev[i];
+    }
 
     newX[newIndex] = oldX[oldIndex];
-    newIndex += step;
+    newIndex += blockstep;
   }
 }
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-void F77_NAME(cwork_gpu, CWORK_GPU) (f_double *y, f_int yOrder, f_int *yDims, f_int *yInds,
-				     f_double *x1, f_int x1Order, f_int *x1Dims, f_int *x1Inds,
-				     f_double *x2, f_int x2Order, f_int *x2Dims, f_int *x2Inds) {
-  int cInds[MAX_DIMS];
-  int xLds[MAX_DIMS];
-  int xSteps[MAX_DIMS];
-  int yLds[MAX_DIMS];
-  int ySteps[MAX_DIMS];
-  int step, lda, ldb, i, j;
-  int x1Length, x2Length, yLength = 1;
-  int numTransfers, transferLength;
-  int cOrder = 0;
+void _cwork(f_double*  y, f_int ny, f_int*  yDims, f_int*  yInds,
+	    f_double* x1, f_int n1, f_int* x1Dims, f_int* x1Inds,
+	    f_double* x2, f_int n2, f_int* x2Dims, f_int* x2Inds) {
+  int steps[MAX_DIMS];
+  int yIndsP[MAX_DIMS], yDimsP[MAX_DIMS];
+  int x1IndsP[MAX_DIMS], x1DimsP[MAX_DIMS];
+  int x2IndsP[MAX_DIMS], x2DimsP[MAX_DIMS];
+  int step;
+  int lda, ldb;
+  int i, j;
+  int c, k;
+  int size;
+  int nc = (n1 + n2 - ny) / 2;
+  bool isContractedIndex;
 
-  printf("Y order: %d\nY dims: ", yOrder);
-  for(i = 0; i < yOrder; i++)
-    printf("%d ", yDims[i]);
-  printf("\nY inds: ");
-  for(i = 0; i < yOrder; i++)
-    printf("%d", yInds[i]);
+  // determine permutations of x1, x2, and y
+  c = 0;
+  k = 0;
+  for(i = 0; i < n1; i++) {
+    isContractedIndex = false;
 
-  printf("\nX1 order: %d\nX1 dims: ", x1Order);
-  for(i = 0; i < x1Order; i++)
-    printf("%d ", x1Dims[i]);
-  printf("X1 inds: ");
-  for(i = 0; i < x1Order; i++)
-    printf("%d", x1Inds[i]);
-  printf("\n");
-
-
-  // Reverse y, x1, and x2 ordering to match row major
-  for(i = 0; i < yOrder/2; i++) {
-    j = yDims[i];
-    yDims[i] = yDims[yOrder - 1 - i];
-    yDims[yOrder - 1 - i] = j;
-    j = yInds[i];
-    yInds[i] = yInds[yOrder - 1 - i];
-    yInds[yOrder - 1 - i] = j;
-  }
-
-  for(i = 0; i < x1Order/2; i++) {
-    j = x1Dims[i];
-    x1Dims[i] = x1Dims[x1Order - 1 - i];
-    x1Dims[x1Order - 1 - i] = j;
-    j = x1Inds[i];
-    x1Inds[i] = x1Inds[x1Order - 1 - i];
-    x1Inds[x1Order - 1 - i] = j;
-  }
-
-
-  for(i = 0; i < x2Order/2; i++) {
-    j = x2Dims[i];
-    x2Dims[i] = x2Dims[x2Order - 1 - i];
-    x2Dims[x2Order - 1 - i] = j;
-    j = x2Inds[i];
-    x2Inds[i] = x2Inds[x2Order - 1 - i];
-    x2Inds[x2Order - 1 - i] = j;
-  }
-
-  // determine which indices to contract
-  for(i = 0; i < x1Order; i++)
-    for(j = 0; j < x2Order; j++)
-      if(x1Inds[i] == x2Inds[j])
-	cInds[cOrder++] = x1Inds[i];
-
-  // copy x1 into scratch3 and then reorder into scratch1
-  {
-    int cMask[MAX_DIMS] = {0};
-    step = 1;
-    for(i = x1Order - 1; i >= 0; i--) {
-      xLds[i] = step;
-      step *= x1Dims[i];
+    for(j = 0; j < n2; j++) {
+      if(x1Inds[i] == x2Inds[j]) {
+	isContractedIndex = true;
+	x1IndsP[n1 - nc + c] = x1Inds[i];
+	x1DimsP[n1 - nc + c] = x1Dims[i];
+	x2IndsP[c] = x2Inds[j];
+	x2DimsP[c] = x2Dims[j];
+	c++;
+	break;
+      }
     }
-    x1Length = step;
+
+    if(!isContractedIndex) {
+      x1IndsP[k] = x1Inds[i];
+      x1DimsP[k] = x1Dims[i];
+      yIndsP[k] = x1Inds[i];
+      yDimsP[k] = x1Dims[i];
+      k++;
+    }
+  }
+
+  c = 0;
+  for(i = 0; i < n2; i++) {
+    for(j = 0; j < ny; j++) {
+      if(x2Inds[i] == yInds[j]) {
+	x2IndsP[nc + c] = x2Inds[i];
+	x2DimsP[nc + c] = x2Dims[i];
+	yIndsP[k] = yInds[j];
+	yDimsP[k] = yDims[j];
+	k++;
+	c++;
+      }
+    }
+  }
   
-    for(i = 0; i < cOrder; i++) {
-      for(j = 0; j < x1Order; j++) {
-	if(cInds[i] == x1Inds[j]) {
-	  step /= x1Dims[j];
-	  xSteps[j] = step;
-	  cMask[j] = 1;
-	}
+  // copy x1 into scratch3 and then reorder into scratch1
+  step = 1;
+  for(i = 0; i < n1; i++) {
+    for(j = 0; j < n1; j++)
+      if(x1Inds[j] == x1IndsP[i]) {
+	steps[j] = step;
+	break;
       }
-    }
-    lda = step;
-
-    for(i = 0; i < x1Order; i++) {
-      if(cMask[i])
-	continue;
-
-      step /= x1Dims[i];
-      xSteps[i] = step;
-      for(j = 0; j < yOrder; j++)
-	if(yInds[j] == x1Inds[i])
-	  ySteps[j] = step;
-    }
-
-    cudaMemcpyToSymbol(ldDev, xLds, sizeof(int) * x1Order);
-    cudaMemcpyToSymbol(stepDev, xSteps, sizeof(int) * x1Order);
-
-    numTransfers = (x1Length * sizeof(double) + REORDER_TRANSFER_SIZE_MB * 1024 * 1024 - 1) / (REORDER_TRANSFER_SIZE_MB * 1024 * 1024);
-    transferLength = REORDER_TRANSFER_SIZE_MB * 1024 * 1024 / sizeof(double);
-
-    for(i = 0; i < numTransfers; i++) {
-      if(transferLength * i < x1Length) {
-	cudaMemcpyAsync(scratch3 + transferLength * i, x1 + transferLength * i, 
-			imin(transferLength, x1Length - transferLength * i) * sizeof(double), 
-			cudaMemcpyHostToDevice, reorderStreams[i % REORDER_STREAMS]);
-	reorderScatter<<<REORDER_BLOCKS, REORDER_THREADS, 0, reorderStreams[i % REORDER_STREAMS]>>>
-	  (scratch1, scratch3, x1Order, imin(transferLength * (i + 1), x1Length), transferLength * i);
-      }
-    }
-
-    cudaDeviceSynchronize();
+    step *= x1DimsP[i];
   }
+  size = step;      
+
+  cudaMemcpyToSymbol(dimsDev, x1Dims, sizeof(f_int) * n1);
+  cudaMemcpyToSymbol(stepsDev, steps, sizeof(f_int) * n1);
+
+  cudaMemcpy(scratch3, x1, size * sizeof(f_double), cudaMemcpyHostToDevice);
+  reorderScatter<<<REORDER_BLOCKS, REORDER_THREADS>>>(scratch1, scratch3, n1, size);
+  cudaDeviceSynchronize();
 
   // copy x2 into scratch3 and then reorder into scratch2
-  {
-    int cMask[MAX_DIMS] = {0};
-
-    for(i = x2Order - 1; i >= 0; i--) {
-      xLds[i] = step;
-      step *= x2Dims[i];
-    }
-    x2Length = step;
-     
-    step = 1;
-    for(i = cOrder - 1; i >= 0; i--) {
-      for(j = 0; j < x2Order; j++) {
-	if(x2Inds[j] == cInds[i]) {
-	  xSteps[j] = step;
-	  step *= x2Dims[j];
-	  cMask[j] = 1;
-	  break;
-	}
+  step = 1;
+  for(i = 0; i < n2; i++) {
+    for(j = 0; j < n2; j++)
+      if(x2Inds[j] == x2IndsP[i]) {
+	steps[j] = step;
+	break;
       }
-    }
-    ldb = step;
-  
-    int ys = lda;
-    for(i = x2Order - 1; i >= 0; i--) {
-      if(cMask[i])
-	continue;
-
-      xSteps[i] = step;
-      step *= x2Dims[i];
-      for(j = 0; j < yOrder; j++)
-	if(yInds[j] == x2Inds[i]) {
-	  ySteps[j] = ys;
-	  ys *= x2Dims[i];
-	}
-    }
-
-    cudaMemcpyToSymbol(ldDev, xLds, sizeof(int) * x2Order);
-    cudaMemcpyToSymbol(stepDev, xSteps, sizeof(int) * x2Order);
-
-    numTransfers = (x2Length * sizeof(double) + REORDER_TRANSFER_SIZE_MB * 1024 * 1024 - 1) / (REORDER_TRANSFER_SIZE_MB * 1024 * 1024);
-
-    for(i = 0; i < numTransfers; i++) {
-      if(transferLength * i < x2Length) {
-	cudaMemcpyAsync(scratch3 + transferLength * i, x2 + transferLength * i, 
-			imin(transferLength, x2Length - transferLength * i) * sizeof(double), 
-			cudaMemcpyHostToDevice, reorderStreams[i % REORDER_STREAMS]);
-	reorderScatter<<<REORDER_BLOCKS, REORDER_THREADS, 0, reorderStreams[i % REORDER_STREAMS]>>>
-	  (scratch2, scratch3, x2Order, imin(transferLength * (i + 1), x2Length), transferLength * i);
-      }
-    }
-
-    cudaDeviceSynchronize();
+    step *= x2DimsP[i];
   }
-
+  size = step;      
+  
+  cudaMemcpyToSymbol(dimsDev, x2Dims, sizeof(f_int) * n2);
+  cudaMemcpyToSymbol(stepsDev, steps, sizeof(f_int) * n2);
+  
+  cudaMemcpy(scratch3, x2, size * sizeof(f_double), cudaMemcpyHostToDevice);
+  reorderScatter<<<REORDER_BLOCKS, REORDER_THREADS>>>(scratch2, scratch3, n2, size);
+  cudaDeviceSynchronize();
+  
   // dGemm scratch1 and scratch2 into scratch 3
   double alpha = 1.0;
   double beta = 0.0;
-  cublasDgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, lda, x2Length / ldb, ldb, 
-		&alpha, scratch1, lda, scratch2, ldb, &beta, scratch3, lda);
+
+  lda = 1;
+  for(i = 0; i < n1 - nc; i++)
+    lda *= x1DimsP[i];
+
+  ldb = 1;
+  for(i = 0; i < nc; i++)
+    ldb *= x2DimsP[i];
+
+  cublasDgemm(cublasHandle, CUBLAS_OP_N, CUBLAS_OP_N, lda, size / ldb, ldb, 
+	      &alpha, scratch1, lda, scratch2, ldb, &beta, scratch3, lda);
   cudaDeviceSynchronize();
 
   // reorder y from scratch3 to scratch1 and copy back from GPU
-  for(i = yOrder - 1; i >= 0; i--) {
-    yLds[i] = yLength;
-    yLength *= yDims[i];
+  step = 1;
+  for(i = 0; i < ny; i++) {
+    for(j = 0; j < ny; j++)
+      if(yInds[j] == yIndsP[i]) {
+	steps[j] = step;
+	break;
+      }
+    step *= yDimsP[i];
   }
+  size = step;      
 
-  cudaMemcpyToSymbol(ldDev, yLds, sizeof(int) * x2Order);
-  cudaMemcpyToSymbol(stepDev, ySteps, sizeof(int) * x2Order);
+  cudaMemcpyToSymbol(dimsDev, yDims, sizeof(f_int) * ny);
+  cudaMemcpyToSymbol(stepsDev, steps, sizeof(f_int) * ny);
 
-  numTransfers = (yLength * sizeof(double) + REORDER_TRANSFER_SIZE_MB * 1024 * 1024 - 1) / (REORDER_TRANSFER_SIZE_MB * 1024 * 1024);
+  reorderGather<<<REORDER_BLOCKS, REORDER_THREADS>>>(scratch1, scratch3, ny, size);
+  cudaMemcpy(y, scratch1, size * sizeof(f_double), cudaMemcpyDeviceToHost);
   
-  for(i = 0; i < numTransfers; i++) {
-    if(transferLength * i < yLength) {
-      reorderGather<<<REORDER_BLOCKS, REORDER_THREADS, 0, reorderStreams[i % REORDER_STREAMS]>>>
-	(scratch1, scratch3, yOrder, imin(transferLength * (i + 1), yLength), transferLength * i);
-      cudaMemcpyAsync(y + transferLength * i, scratch1 + transferLength * i, 
-		      imin(transferLength, yLength - transferLength * i) * sizeof(double), 
-		      cudaMemcpyDeviceToHost, reorderStreams[i % REORDER_STREAMS]);
-    }
-  }
   cudaDeviceSynchronize();
-}
-}
-void cworkGPU(double* y, int yOrder, int* yDims, int* yInds,
-	      double* x1, int x1Order, int* x1Dims, int* x1Inds,
-	      double* x2, int x2Order, int* x2Dims, int* x2Inds) {
-  F77_NAME(cwork_gpu, CWORK_GPU)(y, yOrder, yDims, yInds,
-				 x1, x1Order, x1Dims, x1Inds,
-				 x2, x2Order, x2Dims, x2Inds);
 }
 
 #endif // __CWORKGPU_CU__
--- src/sia/sip_shared/init_gpu.c
+++ src/sia/sip_shared/init_gpu.c
@@ -7,22 +7,22 @@
 #include "f_types.h"
 #include "f77_name.h"
 
-void F77_NAME(init_gpu, INIT_GPU) (f_int devid) {
+void F77_NAME(init_gpu, INIT_GPU) (f_int* devid) {
   char *filename;
   FILE* file;
 
   filename = getenv("PBS_GPUFILE");
   file = fopen(filename, "r");
-  devid = -1;
-
-  printf("gpufile: %s\n", filename);
+  (*devid) = -1;
 
   if(file != NULL) {
-    while(fscanf(file, "gpu%d", &devid) == 0) {
+    printf("gpufile opened\n");
+    while(fscanf(file, "gpu%d", devid) == 0) {
       fscanf(file, "%*c");
     }
-    if(devid >= 0)
-      cudaSetDevice(devid);
+    printf("Found device id = %d\n", *devid);
+    if(*devid >= 0);
+//      cudaSetDevice(*devid);
   }
 }
 
