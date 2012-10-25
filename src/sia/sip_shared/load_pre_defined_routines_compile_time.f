f90739a9e7f2a998a14811f64bac2e21b1936a09
Victor Lotrich <lotrich@submit2.ufhpc>
2012-10-25 16:28:47 -0400

    working version of 3.0.7 using new sip structure

--- src/sia/sip_shared/load_pre_defined_routines_compile_time.f
+++ src/sia/sip_shared/load_pre_defined_routines_compile_time.f
@@ -16,11 +16,10 @@ C  in the file COPYRIGHT.
       implicit none
 
       integer dummy, load_user_sub
-
-      dummy = load_user_sub('sip_barrier' // char(0),
-     *                       0)
-      dummy = load_user_sub('energy_denominator' // char(0),
-     *                       0)
+c
+      dummy = load_user_sub('sip_barrier' // char(0), 0)
+      dummy = load_user_sub('energy_denominator' // char(0), 0)
+      dummy = load_user_sub('udenominator'//char(0), 0)
       dummy = load_user_sub('energy_product' // char(0),
      *                       0)
       dummy = load_user_sub('energy_reg_denominator' // char(0),
@@ -45,11 +44,14 @@ C  in the file COPYRIGHT.
      *                       0)
       dummy = load_user_sub('list_to_blocks' // char(0),
      *                       0)
-      dummy = load_user_sub('fmult' // char(0), 0)
-      dummy = load_user_sub('der_int_setup' // char(0), 0)
-      dummy = load_user_sub('compute_derivative_integrals'//char(0), 
+      dummy = load_user_sub('fmult' // char(0),
      *                       0)
-      dummy = load_user_sub('server_barrier'//char(0), 0)
+      dummy = load_user_sub('der_int_setup' // char(0),
+     *                       0)
+      dummy = load_user_sub('compute_derivative_integrals'//char(0),
+     *                       0)
+      dummy = load_user_sub('server_barrier'//char(0),
+     *                       0)
       dummy = load_user_sub('scont1'//char(0), 0)
       dummy = load_user_sub('hcont1'//char(0), 0)
       dummy = load_user_sub('dcont2'//char(0), 0)
@@ -58,14 +60,16 @@ C  in the file COPYRIGHT.
       dummy = load_user_sub('eig_sr'//char(0), 0)
       dummy = load_user_sub('eig_sr_inv'//char(0), 0)
       dummy = load_user_sub('copy_fock'//char(0), 0)
-
       dummy = load_user_sub('diis_setup'//char(0), 0)
       dummy = load_user_sub('compute_diis'//char(0), 0)
-      dummy = load_user_sub('write_blocks_to_list'//char(0), 0)
-      dummy = load_user_sub('read_list_to_blocks'//char(0), 0)
+      dummy = load_user_sub('write_blocks_to_list'//char(0), 
+     *             0)
+      dummy = load_user_sub('read_list_to_blocks'//char(0), 
+     *             0)
       dummy = load_user_sub('remove_diagonal'//char(0), 0)
       dummy = load_user_sub('return_diagonal'//char(0), 0)
-      dummy = load_user_sub('fock_denominator'//char(0), 0)
+      dummy = load_user_sub('fock_denominator'//char(0),
+     *            0)
       dummy = load_user_sub('set_flags'//char(0), 0)
       dummy = load_user_sub('set_flags2'//char(0), 0)
       dummy = load_user_sub('der2_comp'//char(0), 0)
@@ -73,36 +77,23 @@ C  in the file COPYRIGHT.
       dummy = load_user_sub('overlap_der'//char(0), 0)
       dummy = load_user_sub('scontxy'//char(0), 0)
       dummy = load_user_sub('hcontxy'//char(0), 0)
-      dummy = load_user_sub('compute_sderivative_integrals'//char(0), 0)
+      dummy = load_user_sub('compute_sderivative_integrals'//char(0),
+     *                       0)
       dummy = load_user_sub('read_hess'//char(0), 0)
       dummy = load_user_sub('return_h1'//char(0), 0)
 
-      dummy = load_user_sub('smon_on'//char(0), 0)
-      dummy = load_user_sub('smon_off'//char(0), 0)
+      dummy = load_user_sub('smon_on'//char(0), 
+     *                       0)
+      dummy = load_user_sub('smon_off'//char(0), 
+     *                       0)
       dummy = load_user_sub('scf_rhf'//char(0), 0)
       dummy = load_user_sub('array_copy'//char(0), 0)
-      dummy = load_user_sub('energy_adenominator' // char(0),
-     *                       0)
-      dummy = load_user_sub('energy_bdenominator' // char(0),
-     *                       0)
-      dummy = load_user_sub('energy_abdenominator' // char(0),
-     *                       0)
-      dummy = load_user_sub('read_grad'//char(0), 0)
-      dummy = load_user_sub('udenominator'//char(0), 0)
-      dummy = load_user_sub('set_index'//char(0), 0)
-      dummy = load_user_sub('remove_single'//char(0), 0)
-      dummy = load_user_sub('remove_double'//char(0), 0)
-      dummy = load_user_sub('remove_single_double'//char(0), 0)
-      dummy = load_user_sub('remove_ssss'//char(0), 0)
-      dummy = load_user_sub('remove_dddd'//char(0), 0)
-      dummy = load_user_sub('remove_xsxs'//char(0), 0)
-      dummy = load_user_sub('remove_xxss'//char(0), 0)
-      dummy = load_user_sub('remove_xdxs'//char(0), 0)
-      dummy = load_user_sub('remove_xsxd'//char(0), 0)
-      dummy = load_user_sub('remove_xxsd'//char(0), 0)
-      dummy = load_user_sub('remove_xdxd'//char(0), 0)
-      dummy = load_user_sub('remove_xxdd'//char(0), 0)
-      dummy = load_user_sub('remove_xs'//char(0), 0)
+      dummy = load_user_sub('energy_adenominator'//char(0),
+     *                       0) 
+      dummy = load_user_sub('energy_bdenominator'//char(0),
+     *                       0) 
+      dummy = load_user_sub('energy_abdenominator'//char(0),
+     *                       0) 
       dummy = load_user_sub('remove_xd'//char(0), 0)
       dummy = load_user_sub('remove_ds'//char(0), 0)
       dummy = load_user_sub('remove_ss'//char(0), 0)
@@ -112,20 +103,24 @@ C  in the file COPYRIGHT.
       dummy = load_user_sub('remove_sd'//char(0), 0)
       dummy = load_user_sub('remove_sx'//char(0), 0)
       dummy = load_user_sub('remove_dx'//char(0), 0)
-      dummy = load_user_sub('copy_ab'//char(0), 0)
-      dummy = load_user_sub('copy_ba'//char(0), 0)
+c     dummy = load_user_sub('copy_ab'//char(0), copy_ab)
+c     dummy = load_user_sub('copy_ba'//char(0), copy_ba)
       dummy = load_user_sub('copy_ff'//char(0), 0)
-      dummy = load_user_sub('open_amp'//char(0), 0)
+c     dummy = load_user_sub('open_amp'//char(0), open_amp)
       dummy = load_user_sub('dump_amp'//char(0), 0)
       dummy = load_user_sub('comp_ovl3c'//char(0), 0)
       dummy = load_user_sub('check_dconf'//char(0), 0)
-      dummy = load_user_sub('dropmo_expand_basis'//char(0), 0)
+      dummy = load_user_sub('dropmo_expand_basis'//char(0), 
+     *                       0)
       dummy = load_user_sub('square_root'//char(0), 0)
       dummy = load_user_sub('norm_fac'//char(0), 0)
       dummy = load_user_sub('return_sval'//char(0), 0)
-      dummy = load_user_sub('return_diagonal4'//char(0), 0)
-      dummy = load_user_sub('symm_force_a'//char(0), 0)
-      dummy = load_user_sub('symm_force_i'//char(0), 0)
+      dummy = load_user_sub('return_diagonal4'//char(0),
+     *                      0)
+      dummy = load_user_sub('symm_force_a'//char(0),
+     *                      0)
+      dummy = load_user_sub('symm_force_i'//char(0),
+     *                      0)
       dummy = load_user_sub('place_sval'//char(0), 0)
       dummy = load_user_sub('place_one'//char(0), 0)
       dummy = load_user_sub('place_one2'//char(0), 0)
@@ -139,42 +134,72 @@ C  in the file COPYRIGHT.
       dummy = load_user_sub('place_one4b'//char(0), 0)
       dummy = load_user_sub('place_one5b'//char(0), 0)
       dummy = load_user_sub('place_one6b'//char(0), 0)
-      dummy = load_user_sub('place_one_dip'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_2'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_3'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_4'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_5'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_6'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_7'//char(0), 0) 
-      dummy = load_user_sub('place_one_dip_8'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_2'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_3'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_4'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_5'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_6'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_7'//char(0), 0) 
-      dummy = load_user_sub('place_one_dea_8'//char(0), 0) 
-      dummy = load_user_sub('eomroot_print'//char(0), 0) 
-      dummy = load_user_sub('eomroot_print_new'//char(0), 0) 
-      dummy = load_user_sub('smooth'//char(0), 0)
-      dummy = load_user_sub('smooth4'//char(0), 0)
-      dummy = load_user_sub('eig_nonsymm'//char(0), 0)
+      dummy = load_user_sub('place_one_dip'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_2'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_3'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_4'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_5'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_6'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_7'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dip_8'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_2'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_3'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_4'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_5'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_6'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_7'//char(0),
+     *                                      0)
+      dummy = load_user_sub('place_one_dea_8'//char(0),
+     *                                      0)
+      dummy = load_user_sub('eomroot_print'//char(0), 0)
+      dummy = load_user_sub('eomroot_print_new'//char(0), 
+     *                       0)
+c     dummy = load_user_sub('smooth'//char(0), smooth)
+c     dummy = load_user_sub('smooth4'//char(0), smooth4)
+      dummy = load_user_sub('eig_nonsymm'//char(0),
+     *                       0)
       dummy = load_user_sub('apply_den2'//char(0), 0)
       dummy = load_user_sub('apply_den4'//char(0), 0)
-      dummy = load_user_sub('apply_den4_nodiag'//char(0), 0)
-      dummy = load_user_sub('energy_tdenominator'//char(0), 0)
-
-      dummy = load_user_sub('compute_aaaa_batch'//char(0), 0)
-      dummy = load_user_sub('compute_aaab_batch'//char(0), 0)
-      dummy = load_user_sub('compute_aabb_batch'//char(0), 0)
-      dummy = load_user_sub('compute_aabc_batch'//char(0), 0)
-      dummy = load_user_sub('compute_abab_batch'//char(0), 0)
-      dummy = load_user_sub('compute_abac_batch'//char(0), 0)
-      dummy = load_user_sub('compute_abcd_batch'//char(0), 0)
-      dummy = load_user_sub('compute_no4c_batch'//char(0), 0)
-      dummy = load_user_sub('remove_atom_rud1'//char(0), 0)
-      dummy = load_user_sub('remove_atom_rud2'//char(0), 0)
+      dummy = load_user_sub('apply_den4_nodiag'//char(0),
+     *                       0)
+      dummy = load_user_sub('energy_tdenominator'//char(0),
+     *                       0)
+      
+c     dummy = load_user_sub('compute_aaaa_batch'//char(0),
+c    *                       compute_aaaa_batch)
+c     dummy = load_user_sub('compute_aaab_batch'//char(0),
+c    *                       compute_aaab_batch)
+      dummy = load_user_sub('compute_aabb_batch'//char(0),
+     *                       0)
+c     dummy = load_user_sub('compute_aabc_batch'//char(0),
+c    *                       compute_aabc_batch)
+c     dummy = load_user_sub('compute_abab_batch'//char(0),
+c    *                       compute_abab_batch)
+c     dummy = load_user_sub('compute_abac_batch'//char(0),
+c    *                       compute_abac_batch)
+      dummy = load_user_sub('compute_abcd_batch'//char(0),
+     *                       0)
+      dummy = load_user_sub('compute_no4c_batch'//char(0),
+     *                       0)
+      dummy = load_user_sub('remove_atom_rud1'//char(0),
+     *                       0)
+      dummy = load_user_sub('remove_atom_rud2'//char(0),
+     *                       0)
       dummy = load_user_sub('der4_comp'//char(0), 0)
       dummy = load_user_sub('set_flags4'//char(0), 0)
       dummy = load_user_sub('timestamp'//char(0), 0)
@@ -192,8 +217,10 @@ C  in the file COPYRIGHT.
       dummy = load_user_sub('get_my_rank'//char(0), 0)
       dummy = load_user_sub('broadcast_array'//char(0), 0)
       dummy = load_user_sub('checkpoint'//char(0), 0)
-      dummy = load_user_sub('get_restart_status'//char(0), 0)
-      dummy = load_user_sub('commit_checkpoint'//char(0), 0)
+      dummy = load_user_sub('get_restart_status'//char(0), 
+     *                       0)
+      dummy = load_user_sub('commit_checkpoint'//char(0), 
+     *                       0)
       dummy = load_user_sub('crash'//char(0), 0)
 C
 C
@@ -203,13 +230,16 @@ C
       dummy = load_user_sub('return_derv_xyz'//char(0), 0)
       dummy = load_user_sub('dipole_moment'//char(0), 0)
       dummy = load_user_sub('second_moment'//char(0), 0)
-      dummy = load_user_sub('energy_ty_denominator'//char(0), 0)
+      dummy = load_user_sub('energy_ty_denominator'//char(0),
+     *                       0)
       dummy = load_user_sub('reorder_energy'//char(0), 0)
       dummy = load_user_sub('return_1st_mom'//char(0), 0)
       dummy = load_user_sub('return_2nd_mom'//char(0), 0)
 
-      dummy = load_user_sub('nuc_dipole_moment'//char(0), 0)
-      dummy = load_user_sub('nuc_dipole_derivative'//char(0), 0)
+      dummy = load_user_sub('nuc_dipole_moment'//char(0),
+     *                       0)
+      dummy = load_user_sub('nuc_dipole_derivative'//char(0),
+     *                       0)
 
       dummy = load_user_sub('return_x'//char(0), 0)
       dummy = load_user_sub('return_y'//char(0), 0)
@@ -223,12 +253,13 @@ C
       dummy = load_user_sub('return_xz'//char(0), 0)
       dummy = load_user_sub('return_yz'//char(0), 0)
 
-      dummy = load_user_sub('print_eom_dens_info'//char(0), 0)
-
-c --------------------------------------------------------------------
-c VFl scf integral routines
-c --------------------------------------------------------------------
-
+      dummy = load_user_sub('print_eom_dens_info'//char(0),
+     +                       0)
+c
+c -------------------------------------------------------------------- 
+c VFL Instructions needed to perform 'fast' scf calculations 
+c -------------------------------------------------------------------- 
+c
       dummy = load_user_sub('compute_batch1'//char(0), 0)
       dummy = load_user_sub('compute_batch2'//char(0), 0)
       dummy = load_user_sub('compute_batch3'//char(0), 0)
@@ -247,16 +278,15 @@ c --------------------------------------------------------------------
       dummy = load_user_sub('compute_ubatch7'//char(0), 0)
       dummy = load_user_sub('compute_ubatch8'//char(0), 0)
 
-
       dummy = load_user_sub('form_iad'//char(0), 0)
       dummy = load_user_sub('form_ibd'//char(0), 0)
 
       dummy = load_user_sub('set_itol'//char(0), 0)
       dummy = load_user_sub('fassign'//char(0), 0)
 c
-c --------------------------------------------------------------------
-c VFL Instructions needed to perform 'super' T calculations
-c --------------------------------------------------------------------
+c -------------------------------------------------------------------- 
+c VFL Instructions needed to perform 'super' T calculations 
+c -------------------------------------------------------------------- 
 c
       dummy = load_user_sub('set_t3blocks_a'//char(0), 0)
       dummy = load_user_sub('set_t3blocks_i'//char(0), 0)
@@ -265,9 +295,9 @@ c
       dummy = load_user_sub('prefetch_on'//char(0), 0)
       dummy = load_user_sub('prefetch_off'//char(0), 0)
 c
-c --------------------------------------------------------------------
-c VFL Instruction needed to perform atomic density guess
-c --------------------------------------------------------------------
+c -------------------------------------------------------------------- 
+c VFL Instruction needed to perform atomic density guess  
+c -------------------------------------------------------------------- 
 c
       dummy = load_user_sub('scf_atom'//char(0), 0) 
 c
@@ -275,7 +305,8 @@ c --------------------------------------------------------------------
 c VFL Instruction needed for printing, FNO, ect.. updated FEB. 29 2012  
 c -------------------------------------------------------------------- 
 c
-      dummy = load_user_sub('print_rel_info'//char(0), 0)
+      dummy = load_user_sub('print_rel_info'//char(0),
+     *                       0)
       dummy = load_user_sub('temp_x'//char(0), 0)
       dummy = load_user_sub('corder'//char(0), 0)
       dummy = load_user_sub('form_fno'//char(0), 0)
@@ -283,6 +314,8 @@ c
       dummy = load_user_sub('maxamp_print'//char(0), 0)
       dummy = load_user_sub('form_fock_rohf'//char(0), 0)
 c
-c
+c -------------------------------------------------------------------- 
+
+
       return
       end
