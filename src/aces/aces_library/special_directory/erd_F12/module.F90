474247bbfbd9cb3628735bfbc08ece9d4fed0e93
Jason Byrd <jasonnbyrd@gmail.com>
2014-02-24 16:46:39 -0500

    added ERD F12 integrals to stack

---
+++ src/aces/aces_library/special_directory/erd_F12/module.F90
@@ -0,0 +1,69 @@
+module rank1
+  double precision x(32400)
+  double precision w(32400)
+end
+
+module rank2
+  double precision x(64800)
+  double precision w(64800)
+end
+
+module rank3
+  double precision x(97200)
+  double precision w(97200)
+end
+
+module rank4
+  double precision x(78400)
+  double precision w(78400)
+end
+
+module rank5
+  double precision x(98000)
+  double precision w(98000)
+end
+
+module rank6
+  double precision x(117600)
+  double precision w(117600)
+end
+
+module rank7
+  double precision x(137200)
+  double precision w(137200)
+end
+
+module rank8
+  double precision x(259200)
+  double precision w(259200)
+end
+
+module rank9
+  double precision x(291600)
+  double precision w(291600)
+end
+
+module rank10
+  double precision x(196000)
+  double precision w(196000)
+end
+
+module rank11
+  double precision x(215600)
+  double precision w(215600)
+end
+
+module rank12
+  double precision x(235200)
+  double precision w(235200)
+end
+
+module rank13
+  double precision x(254800)
+  double precision w(254800)
+end
+
+module rank14
+  double precision x(453600)
+  double precision w(453600)
+end
